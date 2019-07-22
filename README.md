be3615281be85e7ae535fc493ad100a5350786fc
Titus <jordane@hive.fi>
2019-07-22 15:20:28 +0300

    pushed

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Init Repo
+```go
+import "github/jgengo/otac"
+```
+
+**add to your main:**
+
+```go
+const AppName string = ""
+const AppVersion string = ""
+const OTAUrl string = "http://url/check"
+```
+
+**in your main:**
+
+```go
+if err := OTACheck(); err != nil {
+    fmt.Println(err)
+}
+```
---
+++ check.go
@@ -0,0 +1,67 @@
+package otac
+
+import (
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+func getExec() (string, error) {
+	ex, err := os.Executable(); 
+	if err != nil {
+		return "", err
+	}
+
+	fi, err := os.Lstat(ex)
+	if err != nil {
+		return "", err
+	}
+	
+	if fi.Mode()&os.ModeSymlink == os.ModeSymlink {
+		link, _ := filepath.EvalSymlinks(ex)
+		return link, nil
+	} else {
+		return ex, nil
+	}
+}
+
+func OTACheck() error {
+	req, err := http.NewRequest("GET", OTAUrl, nil)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("X-App-Name", AppName)
+	req.Header.Add("X-App-Version", AppVersion)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.Header.Get("Content-Type") == "application/octet-stream" {
+		ex, err := getExec()
+		if err != nil {
+			return err
+		}
+
+		if err := os.Remove(ex); err != nil {
+			return err
+		}
+		
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return err
+		}
+
+		if err := ioutil.WriteFile(ex, body, 0777); err != nil {
+			return err
+		}
+		return nil
+	}
+	return nil
+}
